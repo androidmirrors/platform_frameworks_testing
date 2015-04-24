02339ec8b536ea758b8ab873e9186f354866ebfa
Jake Wharton <jakewharton@gmail.com>
2015-04-24 15:10:38 -0400

    Add Android-specific DisableOnDebug rule.
    This is a rule decorator which disables rules when debugging. The
    most common rule to wrap is a Timeout. JUnit ships with a rule of the
    same name but relies on JVM-specific classes which not only makes it
    ineffective in detecting debugging, but renders it unusable
    altogether.
    
    Change-Id: Ie20af5881a9b684b05461073fd3d36b8e1611c75

---
+++ rules/src/androidTest/java/android/support/test/rule/DisableOnDebugTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Lice`nse is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.support.test.rule;
+
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.Statement;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public final class DisableOnDebugTest {
+    private static class FailOnExecution implements TestRule {
+        public Statement apply(Statement base, Description description) {
+            return new Statement() {
+                @Override
+                public void evaluate() throws Throwable {
+                    throw new UnsupportedOperationException();
+                }
+            };
+        }
+    }
+
+    private static class ToggleableDisableOnDebug extends DisableOnDebug {
+        private final boolean isDebugging;
+
+        ToggleableDisableOnDebug(TestRule delegate, boolean isDebugging) {
+            super(delegate);
+            this.isDebugging = isDebugging;
+        }
+
+        @Override
+        public boolean isDebugging() {
+            return isDebugging;
+        }
+    }
+
+    @Test public void disablesWhenDebugging() throws Throwable {
+        TestRule rule = new ToggleableDisableOnDebug(new FailOnExecution(), true);
+        final AtomicInteger value = new AtomicInteger();
+        Statement incrementValue = new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                value.incrementAndGet();
+            }
+        };
+        rule.apply(incrementValue, null).evaluate();
+        assertEquals(1, value.get());
+    }
+
+    @Test public void enabledWhenNotDebugging() throws Throwable {
+        TestRule rule = new ToggleableDisableOnDebug(new FailOnExecution(), false);
+        final AtomicInteger value = new AtomicInteger();
+        Statement incrementValue = new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                value.incrementAndGet();
+            }
+        };
+        try {
+            rule.apply(incrementValue, null).evaluate();
+            fail();
+        } catch (UnsupportedOperationException ignored) {
+        }
+    }
+}
