cc42e3837d2f66157bb76f6e6ea0c519772c56e1
AjitSingh <jeetsingh.ajit@gmail.com>
2016-04-23 16:53:44 +0530

    Adding hasBackground() view matcher to check if a given view has expected background, this matcher will work on all views.
    Change-Id: I4365d5f29ea764ff517f91d026bf0c2085134047
    Signed-off-by: AjitSingh <jeetsingh.ajit@gmail.com>

--- espresso/core/src/main/java/android/support/test/espresso/matcher/ViewMatchers.java
+++ espresso/core/src/main/java/android/support/test/espresso/matcher/ViewMatchers.java
@@ -21,6 +21,11 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 import static org.hamcrest.Matchers.is;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.test.espresso.util.HumanReadables;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
@@ -29,6 +34,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,12 +56,15 @@ import org.hamcrest.Matchers;
 import org.hamcrest.StringDescription;
 import org.hamcrest.TypeSafeMatcher;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 /**
  * A collection of hamcrest matchers that match {@link View}s.
  */
 public final class ViewMatchers {
+
+  private static final String TAG = "ViewMatchers";
 
   private ViewMatchers() {}
 
@@ -1160,4 +1169,62 @@ public final class ViewMatchers {
     };
   }
 
+  /**
+   * Returns a matcher that matches {@link android.view.View} based on background resource.
+   *<p>
+   * Note: This method compares images at a pixel level and might have significant performance
+   * implications for larger bitmaps.
+   * </p>
+   */
+  public static Matcher<View> hasBackground(final int drawableId) {
+    return new TypeSafeMatcher<View>() {
+      @Override
+      protected boolean matchesSafely(View view) {
+        return assertDrawable(view.getBackground(), drawableId, view);
+      }
+
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("has background with drawable ID: " + drawableId);
+      }
+    };
+  }
+
+  private static boolean assertDrawable(Drawable actual, int expectedId, View v) {
+    if (null == actual || !(actual instanceof BitmapDrawable)) {
+      return false;
+    }
+
+    Bitmap expectedBitmap = null;
+    try {
+      expectedBitmap = BitmapFactory.decodeResource(v.getContext().getResources(), expectedId);
+      if (Build.VERSION.SDK_INT >= 12) {
+        return ((BitmapDrawable) actual).getBitmap().sameAs(expectedBitmap);
+      } else {
+        return compareBitmaps(((BitmapDrawable) actual).getBitmap(), expectedBitmap);
+      }
+    } catch (OutOfMemoryError error) {
+      Log.e(TAG, error.getMessage(), error.getCause());
+      return false;
+
+    } finally {
+      if (expectedBitmap != null) {
+        expectedBitmap.recycle();
+        expectedBitmap = null;
+      }
+    }
+  }
+
+  private static boolean compareBitmaps(Bitmap img1, Bitmap img2) {
+    if (img1.getWidth() == img2.getWidth() && img1.getHeight() == img2.getHeight()) {
+      int[] img1Pixels = new int[img1.getWidth() * img1.getHeight()];
+      int[] img2Pixels = new int[img2.getWidth() * img2.getHeight()];
+
+      img1.getPixels(img1Pixels, 0, img1.getWidth(), 0, 0, img1.getWidth(), img1.getHeight());
+      img2.getPixels(img2Pixels, 0, img2.getWidth(), 0, 0, img2.getWidth(), img2.getHeight());
+
+      return Arrays.equals(img1Pixels, img2Pixels);
+    }
+    return false;
+  }
 }
