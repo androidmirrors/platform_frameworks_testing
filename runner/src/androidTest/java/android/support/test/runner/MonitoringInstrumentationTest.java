f81ef56f9f41bbb774f43e8160550efbbf52cd3e
Manish Goyal <manishgo@thoughtworks.com>
2016-04-26 23:18:18 -0400

    Enhancing MonitoringInstrumentation that enables the users to provide instance of Activity under test
    Added a couple of methods in MonitoringInstrumentation that can be used to provide InterceptingActivityFactory and reset it. An InterceptingActivityFactory can be used to create an Activity object provided it supports creating it. If set, InterceptingActivityFactory is used by MonitoringInstrumentation to return Activity object on call of newActivity() method, otherwise default mechanism is used to create Activity object.
    Also added a constructor in ActivityTestRule that accepts SingleActivityFactory, this SingleActivityFactory is set in the instrumentation before test is run and reset in the teardown. This provides a facility of testing the activity in isolation with the outer world by overriding the methods e.g. startService(), sendBroadcast() etc to do whatever test wants to do.
    
    Change-Id: Ic67dfa4d1bee803854a1f6583998ccdb48e3cce2

---
+++ runner/src/androidTest/java/android/support/test/runner/MonitoringInstrumentationTest.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.test.runner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.test.runner.intercepting.InterceptingActivityFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+
+@RunWith(AndroidJUnit4.class)
+public class MonitoringInstrumentationTest {
+
+    private MonitoringInstrumentation instrumentation;
+
+    @Before
+    public void setUp() throws Exception {
+        instrumentation = (MonitoringInstrumentation) getInstrumentation();
+    }
+
+    @Test
+    public void shouldUseDefaultMechanismForCreatingActivityIfInterceptingActivityFactoryNotSet()
+            throws Exception {
+        final Class<TestActivity> testActivityClass = TestActivity.class;
+        final AtomicReference<Activity> activity = new AtomicReference<>();
+        retrieveActivityOnMainThread(testActivityClass, activity);
+
+        instrumentation.waitForIdleSync();
+        assertThat(activity.get(), instanceOf(TestActivity.class));
+    }
+
+    @Test
+    public void shouldCreateNewActivityUsingInterceptingActivityFactoryIfItCanCreate()
+        throws Exception {
+        final Class<TestActivity> testActivityClass = TestActivity.class;
+
+        final AtomicReference<Activity> testActivityReference = new AtomicReference<>();
+        final TestActivity myTestActivity = mock(TestActivity.class);
+        instrumentation.interceptActivityUsing(interceptingActivityFactory(myTestActivity, true));
+
+        retrieveActivityOnMainThread(testActivityClass, testActivityReference);
+
+        instrumentation.waitForIdleSync();
+        assertThat(testActivityReference.get(), sameInstance((Activity) myTestActivity));
+    }
+
+    @Test
+    public void shouldNotCreateNewActivityUsingInterceptingActivityFactoryIfItCannotCreate()
+        throws Exception {
+        final Class<TestActivity> testActivityClass = TestActivity.class;
+
+        final AtomicReference<Activity> testActivityReference = new AtomicReference<>();
+        final TestActivity myTestActivity = mock(TestActivity.class);
+        instrumentation.interceptActivityUsing(interceptingActivityFactory(myTestActivity, false));
+
+        retrieveActivityOnMainThread(testActivityClass, testActivityReference);
+
+        instrumentation.waitForIdleSync();
+        assertThat(testActivityReference.get(), not(sameInstance((Activity) myTestActivity)));
+    }
+
+    @Test
+    public void shouldNotCreateNewActivityUsingInterceptingActivityFactoryIfReset()
+        throws Exception {
+        final Class<TestActivity> testActivityClass = TestActivity.class;
+        final AtomicReference<Activity> activity = new AtomicReference<>();
+
+        final TestActivity myTestActivity = mock(TestActivity.class);
+        instrumentation.interceptActivityUsing(interceptingActivityFactory(myTestActivity, true));
+        instrumentation.useDefaultInterceptingActivityFactory();
+        retrieveActivityOnMainThread(testActivityClass, activity);
+
+        instrumentation.waitForIdleSync();
+        assertThat(activity.get(), not(sameInstance((Activity) myTestActivity)));
+    }
+
+    private void retrieveActivityOnMainThread(final Class<TestActivity> activityClass,
+                                          final AtomicReference<Activity> activityAtomicReference) {
+        instrumentation.runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    activityAtomicReference.set(instrumentation.newActivity(
+                        activityClass.getClassLoader(), activityClass.getName(), new Intent()));
+                } catch (Exception ex) {
+                    fail(ex.getMessage());
+                }
+            }
+        });
+    }
+
+    @NonNull
+    private <T extends Activity> InterceptingActivityFactory interceptingActivityFactory(
+        final T activity, final boolean shouldCreate) {
+        return new InterceptingActivityFactory() {
+            @Override
+            public boolean shouldIntercept(ClassLoader classLoader, String className,
+                                           Intent intent) {
+                return shouldCreate;
+            }
+
+            @Override
+            public Activity create(ClassLoader classLoader, String className, Intent intent) {
+                return activity;
+            }
+        };
+    }
+
+    public static class TestActivity extends Activity {
+
+    }
+
+    public static class SomeOtherActivity extends Activity {
+
+    }
+}
