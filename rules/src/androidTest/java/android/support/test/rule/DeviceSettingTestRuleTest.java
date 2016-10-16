5a687649f1d8e679df8b05e75dd80612c7017b48
Prateek Jain <prateek.jain23021995@gmail.com>
2016-10-16 19:39:45 +0530

    creating a DeviceSettingTestRule to run a test under given DeviceSetting.
    This feature will allow devlopers to test any activity under a certain device setting.
    He/She can verify the changing behaviour of an application which actually depends on the Device's different setting.
    
    Change-Id: I01bb64d7da7228bd9213dcdb78c3799e948ab22f
    Signed-off-by: Prateek Jain <prateek.jain23021995@gmail.com>

---
+++ rules/src/androidTest/java/android/support/test/rule/DeviceSettingTestRuleTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.test.rule;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.annotation.WithSetting;
+import android.support.test.internal.setting.TwelveHourFormatSetting;
+import android.support.test.runner.AndroidJUnit4;
+import android.text.format.DateFormat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.runner.JUnitCore.runClasses;
+
+
+@RunWith(AndroidJUnit4.class)
+public class DeviceSettingTestRuleTest {
+
+
+  private static Context context;
+
+  @Before
+  public void setUp() throws Exception {
+    context = InstrumentationRegistry.getTargetContext();
+    applyTwentyFourHourFormat();
+  }
+
+  public static class TwelveHourTimeFormatTest {
+    @Rule
+    public TestRule rule = new DeviceSettingTestRule();
+
+    @Before
+    public void setUp() throws Exception {
+      verifyTwelveHourFormat();
+    }
+
+    @Test
+    @WithSetting(TwelveHourFormatSetting.class)
+    public void shouldRunUnderTwelveHourTimeFormatSetting() throws Exception {
+      verifyTwelveHourFormat();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+      verifyTwelveHourFormat();
+    }
+  }
+
+  @Test
+  public void shouldApplyGivenSettingBeforeTestAndResetSettingAfterTest() throws Exception {
+    verifyTwentyFourHourFormat();
+    Result result = runClasses(TwelveHourTimeFormatTest.class);
+    assertThat(result.getFailureCount(), is(0));
+    verifyTwentyFourHourFormat();
+  }
+
+  private static void verifyTwentyFourHourFormat() {
+    assertTrue(DateFormat.is24HourFormat(context));
+  }
+
+  private static void verifyTwelveHourFormat() {
+    assertFalse(DateFormat.is24HourFormat(context));
+  }
+
+  private void applyTwentyFourHourFormat() {
+    Settings.System.putString(context.getContentResolver(), Settings.System.TIME_12_24, "24");
+  }
+}
